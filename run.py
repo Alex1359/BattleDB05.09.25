f4be7171558fc6b8fca50a13768769f7f5487c94
Alex1359 <160641422+Alex1359@users.noreply.github.com>
2025-09-05 12:28:07 +0300

    Update run.py

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent))
 import os
 from app import create_app, db
+from app import app
 
 # Исправленный путь с обработкой отсутствующей PYTHONPATH
 app_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'app'))
@@ -10,5 +11,7 @@ os.environ['PYTHONPATH'] = app_path + os.pathsep + os.environ.get('PYTHONPATH', 
 
 app = create_app()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
